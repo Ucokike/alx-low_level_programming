a23264c0c8910f78743d57203e6b8d6041a9271d
Ucokike <okikeuchennac@gmail.com>
2022-09-08 21:05:28 -0700

    commit for 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,23 @@
 #include <unistd.h>
 
 /**
- * main- this progam prints a text
  *
- * return is always 1 to be sucessful
- */
+ * * main - Prints a text
+ *
+ * *
+ *
+ * * Return: Always 1 (Success)
+ *
+ * */
+
 int main(void)
+
 {
-	write(2, "and that peice of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-	return(1);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar,
+
+			2015-10-19\n", 59);
+
+	return (1);
+
 }
