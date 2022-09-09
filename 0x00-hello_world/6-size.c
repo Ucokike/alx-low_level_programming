94bdf27aedde3ea5e338eb18a1bba7fd43114eee
Ucokike <okikeuchennac@gmail.com>
2022-09-08 20:32:36 -0700

    correction for 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main- this should print the size of many var files
  *
- * return- it should always return 0 (successful)
- */
+ * * main -thhis program Prints the sizeof many var types
+ *
+ * *
+ *
+ * * Return:it should Always 0 (Successful)
+ *
+ * */
+
 int main(void)
+
 {
+
 	char a;
+
 	int b;
+
 	long int c;
+
 	long long int d;
+
 	float e;
 
-	printf("Size of char: %d byte(s)\n", sizeof(a));
-	printf("Size of int: %d byte(s)\n", sizeof(b));
-	printf("Size of long int: %d byte(s)\n", sizeof(c));
-	printf("Size of long long int: %d byte(s)\n", sizeof(d));
-	printf("Size of a float: %d byte(s)\n", sizeof(e));
-	return(0;);
-}
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
 
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+
+	return (0);
+
+}
