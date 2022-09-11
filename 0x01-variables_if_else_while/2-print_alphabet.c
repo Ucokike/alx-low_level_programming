1ca6263f8e8e242b54df9fcf4d83081e1ea2478a
Ucokike <okikeuchennac@gmail.com>
2022-09-11 16:30:38 -0700

    commit for task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,35 +1,19 @@
 #include <stdio.h>
-
-
-
 /**
  *
- *  * main - lowercase alphabets
+ *  * main -prints lowercase alphabets
  *
  *   *
  *
- *    * Return: Always (Success)
+ *    * Return: Always (Successful)
  *
  *     */
 
 int main(void)
-
 {
-
-		char c;
-
-
-
-			for (c = 'a'; c <= 'z'; c++)
-
-						putchar(c);
-
-
-
-				putchar('\n');
-
-					return (0);
-
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
 }
-
-
