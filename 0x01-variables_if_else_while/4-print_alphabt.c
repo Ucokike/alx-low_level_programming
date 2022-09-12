6dcbb40820c8a07f958221024172425e0b08d85c
Ucokike <okikeuchennac@gmail.com>
2022-09-11 18:41:11 -0700

    good commit for task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 /**
- *
- ** main -this programme Ommits some alphabets
- *
- **
- *
- ** Return: Always (Successful)
- *
- **/
-
+* main -this programme Ommits some alphabets
+* Return: Always (Successful)
+*
+*/
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		if (c != 'e' && c != 'q')
