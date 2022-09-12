b2ae84ed8bef416032e9709a9fece810bcadbe04
Ucokike <okikeuchennac@gmail.com>
2022-09-11 18:36:19 -0700

    good commit for task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 /**
- *
- * * main -prints lowercase and uppercase
- *
- * *
- *
- * * Return: Always (Successful)
- *
- * */
+*
+* main -prints lowercase and uppercase
+*
+* Return: Always (Successful)
+*/
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	putchar(c);
