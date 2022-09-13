b87e9d18a4717d08bc8358281332ddb05fd90594
Ucokike <okikeuchennac@gmail.com>
2022-09-12 20:08:48 -0700

    commit for 1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
- * main- this would print a text
- *
- * return: 0 always (successful)
- */
+*main - this would print a text
+*
+*return:  always 0 (successful)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
-
