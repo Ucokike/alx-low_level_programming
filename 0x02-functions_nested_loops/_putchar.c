a5bcfd1069231932bba9218bdece9c7e8b3c8158
Ucokike <okikeuchennac@gmail.com>
2022-09-14 10:54:17 -0700

    commit of tsk 0

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
 #include<unistd.h>
 
 /**
- * _putchar - this program writes the character c to stdout
+ * _putchar- writes the character c to stdout
  * @c: the character to print
  *
- * return: 1 (success)
- * on error, -1 is returned and erno is set appropriately
+ * Return: 1 (success)
+ * on error, -1 is returned, and erno is set appropriately.
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
