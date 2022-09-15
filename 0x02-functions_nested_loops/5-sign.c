9da1bb6a03beeab5c168c4c8891f3d8ac5e8ca10
Ucokike <okikeuchennac@gmail.com>
2022-09-14 19:19:32 -0700

    good commit for 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,16 +1,25 @@
 #include "main.h"
 /**
- * _isalpha - function to check if c is a letter, lowercase or uppercase
- * @c: is the int that will use for the argument of the function
+ * print_sign - function to check for a sign of a number
+ * @n:  is the int that will use for the argument of the function
  * Return: 0
  */
-int _isalpha(int c)
+int print_sign(int n)
 
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (n > 0)
 	{
+	_putchar('+');
 	return (1);
 	}
+	else if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
 	else
+	{
+	_putchar('0');
 	return (0);
+	}
 }
