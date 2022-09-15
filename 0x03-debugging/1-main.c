81b600413fef899642f11c86e5e2c69ee5c9dcc7
Ucokike <okikeuchennac@gmail.com>
2022-09-15 06:20:29 -0700

    task 1 commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  * Return: 0
  *
  */
-
 int main(void)
-
 {
 	int i;
 
@@ -16,11 +13,8 @@ int main(void)
 /*
  *	while (i < 10)
  *	{
- *
  *	putchar(i);
- *
  *	}
- *
  */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
