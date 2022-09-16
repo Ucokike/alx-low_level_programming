2015af785a280b269ae94a2b1e826a58bb385a93
Ucokike <okikeuchennac@gmail.com>
2022-09-16 08:16:25 -0700

    commit for task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * mul -this multiplies two integers
+ * @a: the first input
+ * @b: the second input
+ * Return: product of a & b
+ */
+
+int mul(int a, int b)
+
+{
+	int product = a * b;
+
+	return (product);
+}
