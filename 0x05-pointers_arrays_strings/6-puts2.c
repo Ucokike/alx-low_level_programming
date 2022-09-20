2b3ca803a11f465cc537dfdd0f75c269f6a316d1
Ucokike <okikeuchennac@gmail.com>
2022-09-20 15:14:36 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character
+ * @str: string
+ * Return: Uche's code
+ */
+void puts2(char *str)
+{
+	int p = 0;
+
+	while (*(str + p) != '\0')
+	{
+		if (p % @ == 0)
+			putchar(*(str + p));
+		p++;
+	}
+	putchar(10)
+}
