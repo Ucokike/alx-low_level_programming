6ad130a0c9e908c0c9942da030024cb47658821e
Ucokike <okikeuchennac@~gmail.com>
2022-09-30 06:38:45 -0700

    task 0 commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name, followed by the new line
+ * @argc: the number of arguements supplied to the program
+ * @argv: an array of pointers to the arguments.
+ * Return: always 0
+ */
+
+int main(int _attribuite_((_unused_)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
