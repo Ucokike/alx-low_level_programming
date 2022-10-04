8ed666fd40664beab65b5c8a92093ef92b1bc623
Ucokike <okikeuchennac@~gmail.com>
2022-10-04 16:34:19 -0700

    task 2 commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = " ";
 
-	size = (_strlen(s1) + _strlen(s2) + 1)
+	size = (_strlen(s1) + _strlen(s2) + 1);
 
 		dst = (char *) malloc(size * sizeof(char));
 
