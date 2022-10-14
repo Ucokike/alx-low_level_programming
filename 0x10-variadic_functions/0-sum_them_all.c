d926817990492fd61443953f20928ca20f508486
Ucokike <okikeuchennac@gmail.com>
2022-10-14 14:30:28 -0700

    task 0 commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,12 +3,12 @@
 /**
  * sum_them_all - sums all the numbers
  * @n: the numbers
- * Return: zero (always success)
+ * Return: sum of all its parameters
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
+	va_list valist;
 	unsigned int i = 0;
 	int sum = 0;
 
@@ -22,5 +22,5 @@ int sum_them_all(const unsigned int n, ...)
 		sum += va_arg(valist, int);
 	va_end(valist);
 
-	return (0);
+	return (sum);
 }
