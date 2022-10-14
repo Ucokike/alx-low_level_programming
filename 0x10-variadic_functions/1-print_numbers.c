028f3fde766aeae84f2f1538163000c4f3f6b8d6
Ucokike <okikeuchennac@gmail.com>
2022-10-14 15:22:28 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(nums, int));
 
 		if (index != (n - 1) && seperator != NULL)
-			printf('%s' seperator);
+			printf("%s", seperator);
 	}
 	printf("\n");
 
