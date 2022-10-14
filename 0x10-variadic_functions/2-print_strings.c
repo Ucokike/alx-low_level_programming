9440a99ac12ee42ae7a9982c825f7d09ec201e51
Ucokike <okikeuchennac@gmail.com>
2022-10-14 15:55:54 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,7 +8,6 @@
  * @n: number of strings to be passed to the function
  * Return: no return
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
@@ -19,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(valist, char*);
+		str = va_arg(valist, char *);
 
 		if (str)
 			printf("%s", str);
